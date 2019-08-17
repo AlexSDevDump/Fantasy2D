9944024ad6be686611884a0facaf168d68d43ce6
Alex Sampson <32361325+15015483Alex@users.noreply.github.com>
2019-08-18 00:41:52 +0100

    Multiple Interaction Fix
    Checks for closest interaction when multiple present

--- Assets/Scripts/Characters/PlayerInteraction.cs
+++ Assets/Scripts/Characters/PlayerInteraction.cs
@@ -5,25 +5,57 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] private Interaction interaction = null;
+    [SerializeField] private List<Collider2D> interactionList = new List<Collider2D>();
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if(interaction == null)
-        {
-            interaction = coll.GetComponent<Interaction>();
-            if(interaction == null)
-                Debug.LogError("INTERACTION COMPONENT MISSING");
-        }
+        interactionList.Add(coll);
+    }
+
+    void Update()
+    {
+        if (interactionList.Count > 0)
+            GetClosestInteraction();
+        else
+            interaction = null;
     }
 
     void OnTriggerExit2D(Collider2D coll)
     {
-        interaction = null;
+        interactionList.Remove(coll);
     }
 
     public void CallInteraction()
     {
         if (interaction != null) { interaction.InteractWithObject(transform.root.gameObject); }
         else { Debug.Log("No object in range"); }
+    }
+
+    private void GetClosestInteraction()
+    {
+        Collider2D closestInteract = null;
+        if(interactionList.Count != 0)
+        {
+            foreach(Collider2D t in interactionList)
+            {
+                if (closestInteract == null) { closestInteract = t; }
+                else
+                {
+                    float dist = Vector3.Distance(t.bounds.min, transform.position);
+                    if(dist < Vector3.Distance(closestInteract.bounds.min, gameObject.GetComponent<Collider2D>().bounds.min))
+                    {
+                        closestInteract = t;
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("INTERACTION LIST EMPTY");
+        }
+
+        interaction = closestInteract.GetComponent<Interaction>();
+        if (interaction == null)
+            Debug.LogError("INTERACTION COMPONENT MISSING");
     }
 }
